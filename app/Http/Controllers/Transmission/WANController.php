0984b1ecb4fd8719c3d994fd157afa10e057f586
Amr Mahmoud Morsy <ammorsy@MOBCORP.INTRT.COM>
2023-11-11 22:56:43 +0200

    updating string reg

--- app/Http/Controllers/Transmission/WANController.php
+++ app/Http/Controllers/Transmission/WANController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Transmission\WAN;
 use Illuminate\Support\Facades\Validator;
 
+use function PHPUnit\Framework\countOf;
 
 class WANController extends Controller
 {
@@ -15,84 +16,85 @@ class WANController extends Controller
     {
         $validator = Validator::make($request->all(), [
             "site_code" => "required|exists:sites,site_code",
-           
+
         ]);
         if ($validator->fails()) {
             return response()->json([
                 "errors" => $validator->getMessageBag()
             ], 422);
         } else {
-            $validated=$validator->validated();
-            $site = Site::where("site_code",$validated["site_code"])->first();
+            $validated = $validator->validated();
+            $site = Site::where("site_code", $validated["site_code"])->first();
             $WANS = $site->wans;
+            if (count($WANS) > 0) {
+                return response()->json([
+                    "message" => "data found",
+                    "WANS" => $WANS
 
-            return response()->json([
-                $WANS
-            ], 200);
+                ], 200);
+            } else {
+                return response()->json([
+                    "message" => "data not found",
+
+
+                ], 200);
+            }
         }
-
-
-      
-
     }
     public function updateSiteWAN(Request $request)
     {
         $validator = Validator::make($request->all(), [
             "site_code" => "required|exists:sites,site_code",
-            "reporting_date"=>"required|date",
-            "clearance_date"=>"nullable|date|after_or_equal:reporting_date",
-            "network_element"=>["required","max:100","regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
-            "far_end"=>["required","max:100","regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
-            "office"=>["required","regex:/^Maadi|Shrouk|New Cairo|Haram|Gisr El Suez|Shoubra|Mohandseen|October|Helwan|New Capital|Nasr City$/"],
-            "status"=>["required","regex:/^Solved|Pending$/"],
-            "ATST_feedback"=>["nullable","max:200","regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
-            "maintenance_feedback"=>["nullable","max:200","regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
+            "reporting_date" => "required|date",
+            "clearance_date" => "nullable|date|after_or_equal:reporting_date",
+            "network_element" => ["required", "max:100", "regex:/^[a-zA-Z0-9_+-@%&# \/]+$/"],
+            "far_end" => ["required", "max:100", "regex:/^[a-zA-Z0-9_+-@%&# \/]+$/"],
+            "office" => ["required", "regex:/^Maadi|Shrouk|New Cairo|Haram|Gisr El Suez|Shoubra|Mohandseen|October|Helwan|New Capital|Nasr City$/"],
+            "status" => ["required", "regex:/^Solved|Pending$/"],
+            "ATST_feedback" => ["nullable", "max:200", "regex:/^[a-zA-Z0-9_+-@%&# \/]+$/"],
+            "maintenance_feedback" => ["nullable", "max:200", "regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
 
 
-           
+
         ]);
         if ($validator->fails()) {
             return response()->json([
                 "errors" => $validator->getMessageBag()
             ], 422);
         } else {
-            $validated=$validator->validated();
-            $WAN=WAN::where("site_code",$validated["site_code"])->first();
-            $WAN=$WAN->update($validated);
-            $WAN=WAN::where("site_code",$validated["site_code"])->first();
-            return response($WAN,200);
+            $validated = $validator->validated();
+            $WAN = WAN::where("site_code", $validated["site_code"])->first();
+            $WAN = $WAN->update($validated);
+            $WAN = WAN::where("site_code", $validated["site_code"])->first();
+            return response($WAN, 200);
         }
-
     }
     public function storeSiteWAN(Request $request)
     {
         $validator = Validator::make($request->all(), [
             "site_code" => "required|exists:sites,site_code",
-            "reporting_date"=>"required|date",
-            "clearance_date"=>"nullable|date|after_or_equal:reporting_date",
-            "network_element"=>["required","max:100","regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
-            "far_end"=>["required","max:100","regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
-            "office"=>["required","regex:/^Maadi|Shrouk|New Cairo|Haram|Gisr El Suez|Shoubra|Mohandseen|October|Helwan|New Capital|Nasr City$/"],
-            "status"=>["required","regex:/^Solved|Pending$/"],
-            "ATST_feedback"=>["nullable","max:200","regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
-            "maintenance_feedback"=>["nullable","max:200","regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
+            "reporting_date" => "required|date",
+            "clearance_date" => "nullable|date|after_or_equal:reporting_date",
+            "network_element" => ["required", "max:100", "regex:/^[a-zA-Z0-9_+-@%&# \/]+$/"],
+            "far_end" => ["required", "max:100", "regex:/^[a-zA-Z0-9_+-@%&# \/]+$/"],
+            "office" => ["required", "regex:/^Maadi|Shrouk|New Cairo|Haram|Gisr El Suez|Shoubra|Mohandseen|October|Helwan|New Capital|Nasr City$/"],
+            "status" => ["required", "regex:/^Solved|Pending$/"],
+            "ATST_feedback" => ["nullable", "max:200", "regex:/^[a-zA-Z0-9_+-@%&# \/]+$/"],
+            "maintenance_feedback" => ["nullable", "max:200", "regex:/^[a-zA-Z0-9_+-@%&# \/]+$/"],
 
 
-           
+
         ]);
         if ($validator->fails()) {
             return response()->json([
                 "errors" => $validator->getMessageBag()
             ], 422);
         } else {
-            $validated=$validator->validated();
-            $WAN=WAN::create($validated);
-           
-          
-            return response($WAN,200);
-        }
+            $validated = $validator->validated();
+            $WAN = WAN::create($validated);
 
+
+            return response($WAN, 200);
+        }
     }
-   
-   
 }
--- app/Http/Controllers/Transmission/XPICController.php
+++ app/Http/Controllers/Transmission/XPICController.php
@@ -10,89 +10,94 @@ use App\Models\Transmission\XPIC;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
+use function PHPUnit\Framework\countOf;
+
 class XPICController extends Controller
 {
     public function getSiteXPICS(Request $request)
     {
         $validator = Validator::make($request->all(), [
             "site_code" => "required|exists:sites,site_code",
-           
+
         ]);
         if ($validator->fails()) {
             return response()->json([
                 "errors" => $validator->getMessageBag()
             ], 422);
         } else {
-            $validated=$validator->validated();
-            $site = Site::where("site_code",$validated["site_code"])->first();
+            $validated = $validator->validated();
+            $site = Site::where("site_code", $validated["site_code"])->first();
             $XPICS = $site->xpics;
+            if (count($XPICS) > 0) {
+                return response()->json([
+                    "message"=>"data found",
+                    "XPICS"=>$XPICS
+                    
+                ], 200);
+            }
+            else{
+                return response()->json([
+                    "message"=>"data not found",
+                   
+                    
+                ], 200);
 
-            return response()->json([
-                $XPICS
-            ], 200);
+            }
         }
-
-
-      
-
     }
     public function updateSiteXPICs(Request $request)
     {
         $validator = Validator::make($request->all(), [
             "site_code" => "required|exists:sites,site_code",
-            "reporting_date"=>"required|date",
-            "clearance_date"=>"nullable|date|after_or_equal:reporting_date",
-            "network_element"=>["required","max:100","regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
-            "far_end"=>["required","max:100","regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
-            "office"=>["required","regex:/^Maadi|Shrouk|New Cairo|Haram|Gisr El Suez|Shoubra|Mohandseen|October|Helwan|New Capital|Nasr City$/"],
-            "status"=>["required","regex:/^Solved|Pending$/"],
-            "ATST_feedback"=>["nullable","max:200","regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
-            "maintenance_feedback"=>["nullable","max:200","regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
+            "reporting_date" => "required|date",
+            "clearance_date" => "nullable|date|after_or_equal:reporting_date",
+            "network_element" => ["required", "max:100", "regex:/^[a-zA-Z0-9_+-@%&# \/]+$/"],
+            "far_end" => ["required", "max:100", "regex:/^[a-zA-Z0-9_+-@%&# \/]+$/"],
+            "office" => ["required", "regex:/^Maadi|Shrouk|New Cairo|Haram|Gisr El Suez|Shoubra|Mohandseen|October|Helwan|New Capital|Nasr City$/"],
+            "status" => ["required", "regex:/^Solved|Pending$/"],
+            "ATST_feedback" => ["nullable", "max:200", "regex:/^[a-zA-Z0-9_+-@%&# \/]+$/"],
+            "maintenance_feedback" => ["nullable", "max:200", "regex:/^[a-zA-Z0-9_+-@%&# \/]+$/"],
 
 
-           
+
         ]);
         if ($validator->fails()) {
             return response()->json([
                 "errors" => $validator->getMessageBag()
             ], 422);
         } else {
-            $validated=$validator->validated();
-            $XPIC=XPIC::where("site_code",$validated["site_code"])->first();
-            $XPIC=$XPIC->update($validated);
-            $XPIC=XPIC::where("site_code",$validated["site_code"])->first();
-            return response()->json([$XPIC],200);
+            $validated = $validator->validated();
+            $XPIC = XPIC::where("site_code", $validated["site_code"])->first();
+            $XPIC = $XPIC->update($validated);
+            $XPIC = XPIC::where("site_code", $validated["site_code"])->first();
+            return response()->json([$XPIC], 200);
         }
-
     }
     public function storeSiteXPICs(Request $request)
     {
         $validator = Validator::make($request->all(), [
             "site_code" => "required|exists:sites,site_code",
-            "reporting_date"=>"required|date",
-            "clearance_date"=>"nullable|date|after_or_equal:reporting_date",
-            "network_element"=>["required","max:100","regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
-            "far_end"=>["required","max:100","regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
-            "office"=>["required","regex:/^Maadi|Shrouk|New Cairo|Haram|Gisr El Suez|Shoubra|Mohandseen|October|Helwan|New Capital|Nasr City$/"],
-            "status"=>["required","regex:/^Solved|Pending$/"],
-            "ATST_feedback"=>["nullable","max:200","regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
-            "maintenance_feedback"=>["nullable","max:200","regex:/^[a-zA-Z0-9_+-@%& \/]+$/"],
+            "reporting_date" => "required|date",
+            "clearance_date" => "nullable|date|after_or_equal:reporting_date",
+            "network_element" => ["required", "max:100", "regex:/^[a-zA-Z0-9_+-@%&# \/]+$/"],
+            "far_end" => ["required", "max:100", "regex:/^[a-zA-Z0-9_+-@%&# \/]+$/"],
+            "office" => ["required", "regex:/^Maadi|Shrouk|New Cairo|Haram|Gisr El Suez|Shoubra|Mohandseen|October|Helwan|New Capital|Nasr City$/"],
+            "status" => ["required", "regex:/^Solved|Pending$/"],
+            "ATST_feedback" => ["nullable", "max:200", "regex:/^[a-zA-Z0-9_+-@%&# \/]+$/"],
+            "maintenance_feedback" => ["nullable", "max:200", "regex:/^[a-zA-Z0-9_+-@%&# \/]+$/"],
 
 
-           
+
         ]);
         if ($validator->fails()) {
             return response()->json([
                 "errors" => $validator->getMessageBag()
             ], 422);
         } else {
-            $validated=$validator->validated();
-            $XPIC=XPIC::create($validated);
-          
-            return response()->json([$XPIC],200);
+            $validated = $validator->validated();
+            $XPIC = XPIC::create($validated);
+
+            return response()->json([$XPIC], 200);
         }
-
     }
-   
-
 }
